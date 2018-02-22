9027fe6a84e4eb95f25af39846f68855518cabc1
zachnamyat <zachnamyat@yahoo.com>
2018-02-22 13:42:28 -0600

    Add files via upload

---
+++ Autoclicker/Autoclicker/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Autoclicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Dur = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(212, 9);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Dur
+            // 
+            this.Dur.FormattingEnabled = true;
+            this.Dur.Location = new System.Drawing.Point(118, 11);
+            this.Dur.Name = "Dur";
+            this.Dur.Size = new System.Drawing.Size(88, 21);
+            this.Dur.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 45);
+            this.Controls.Add(this.Dur);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "AutoClicker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox Dur;
+    }
+}
+
---
+++ Autoclicker/Autoclicker/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using System.Timers;
+
+namespace Autoclicker
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", EntryPoint = "SetCursorPos")]
+        private static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        bool isPressed = false;
+
+        static void Clicker()
+        {
+            System.Threading.Thread.Sleep(1000);
+            SetCursorPos(MousePosition.X, MousePosition.Y);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, MousePosition.X, MousePosition.Y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, MousePosition.X, MousePosition.Y, 0, 0);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartButton.Click += new System.EventHandler(this.button_Click);
+
+            Dur.Items.Add("Seconds");
+            Dur.Items.Add("Milliseconds");
+            Dur.SelectedIndex = 0;
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button thisButton = (Button)sender;
+
+            switch(thisButton.Name)
+            {
+                case "StartButton":
+                    Dur.SelectedIndex = 0;
+                    thisButton.Name = "StopButton";
+                    thisButton.Text = "Stop";
+                    isPressed = true;
+                    while (isPressed == true)
+                    {
+                        //DO NOT START PROGRAM. IT WILL FUCK YOU
+                        //Thread t = new Thread(Clicker);
+                        //t.Start();
+                    }
+                    break;
+
+                case "StopButton":
+                    Dur.SelectedIndex = 1;
+                    thisButton.Name = "StartButton";
+                    thisButton.Text = "Start";
+                    isPressed = false;
+                    break;
+            }
+        }
+    }
+}
